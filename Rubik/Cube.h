c58484beff32f8b802b1cce3da26a8fdf947cae8
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-06-21 18:43:46 -0500

    Added: set_vertices(), set_faces(), to_strign() functions

--- Rubik/Cube.h
+++ Rubik/Cube.h
@@ -1,44 +1,77 @@
 #include <vector>
+#include <map>
 
 #include "Operations.h"
 
+
 class Cube
 {
-private:
-	std::vector<std::vector<double>> coords;
 public:
+	std::vector<unsigned int> VAO;
+	std::vector<unsigned int> VBO;
+	std::vector<vertex> vertices;
+	std::vector<face> faces;
+	std::vector<std::vector<float>> data;
+	
 	Cube();
-	void set_coords(std::vector<double> left_bottom_back, std::vector<double> right_top_front);
-	void rotate(double angle, rotation_axis axis);
+	void set_faces();
+	void set_vertices(vertex left_bottom_back, vertex right_top_front);
+	void rotate(float angle, rotation_axis axis);
 	std::string to_string();
 	~Cube();
-	
 };
 
 
-Cube::Cube() 
+Cube::Cube()
 {
-	coords.resize(8);
-	for (int i = 0; i < 8; i++)
-		coords[i].resize(3);
+	vertices.resize(8);
+	faces.resize(6);
+	data.resize(12);
+	for (int i = 0; i < 12; i++)
+		data[i].resize(9);
+
+	VAO.resize(12);
+	VBO.resize(12);
 }
 
-void Cube::set_coords(std::vector<double> left_bottom_back, std::vector<double> right_top_front)
+
+
+void Cube::set_faces()
 {
-	coords[0] = { left_bottom_back[0], left_bottom_back[1], right_top_front[2] };
-	coords[1] = { right_top_front[0], left_bottom_back[1], right_top_front[2] };
-	coords[2] = { right_top_front[0], left_bottom_back[1], left_bottom_back[2] };
-	coords[3] = left_bottom_back;
-	
-	coords[4] = { left_bottom_back[0], right_top_front[1], right_top_front[2] };
-	coords[5] = right_top_front;
-	coords[6] = { right_top_front[0], right_top_front[1], left_bottom_back[2] };
-	coords[7] = { left_bottom_back[0], right_top_front[1], left_bottom_back[2] };	
+	// x-z plane
+	faces[0] = { vertices[0], vertices[1], vertices[2], vertices[0], vertices[2], vertices[3], faces[0].color_name };
+	faces[1] = { vertices[4], vertices[5], vertices[6], vertices[4], vertices[6], vertices[7], faces[1].color_name };
+	// y-x plane
+	faces[2] = { vertices[0], vertices[1], vertices[5], vertices[0], vertices[5], vertices[4], faces[2].color_name };
+	faces[3] = { vertices[2], vertices[3], vertices[7], vertices[2], vertices[7], vertices[6], faces[3].color_name };
+	// y-z plane
+	faces[4] = { vertices[1], vertices[2], vertices[6], vertices[1], vertices[6], vertices[5], faces[4].color_name };
+	faces[5] = { vertices[3], vertices[0], vertices[4], vertices[3], vertices[4], vertices[7], faces[5].color_name };
+
+	for (int i = 0; i < 6; i++)
+	{
+		data[i * 2] = { faces[i].a1.x, faces[i].a1.y, faces[i].a1.z, faces[i].a2.x, faces[i].a2.y, faces[i].a2.z, faces[i].a3.x, faces[i].a3.y, faces[i].a3.z };
+		data[i * 2 + 1] = { faces[i].b1.x, faces[i].b1.y, faces[i].b1.z, faces[i].b2.x, faces[i].b2.y, faces[i].b2.z, faces[i].b3.x, faces[i].b3.y, faces[i].b3.z };
+	}
 }
 
-void Cube::rotate(double angle, rotation_axis axis)
+void Cube::set_vertices(vertex left_bottom_back, vertex right_top_front)
 {
-	op::rotate(coords, angle, axis);
+	vertices[0] = { left_bottom_back.x, left_bottom_back.y, right_top_front.z };
+	vertices[1] = { right_top_front.x, left_bottom_back.y, right_top_front.z };
+	vertices[2] = { right_top_front.x, left_bottom_back.y, left_bottom_back.z };
+	vertices[3] = left_bottom_back;
+	vertices[4] = { left_bottom_back.x, right_top_front.y, right_top_front.z };
+	vertices[5] = right_top_front;
+	vertices[6] = { right_top_front.x, right_top_front.y, left_bottom_back.z };
+	vertices[7] = { left_bottom_back.x, right_top_front.y, left_bottom_back.z };	
+
+	set_faces();
+}
+
+void Cube::rotate(float angle, rotation_axis axis)
+{
+	op::rotate(vertices, angle, axis);
 }
 				
 				
@@ -47,18 +80,7 @@ std::string Cube::to_string()
 {
 	std::string str = "";
 	for (int i = 0; i < 8; i++)
-	{
-		str += "(";
-		for (int j = 0; j < 3; j++)
-		{
-			str += std::to_string(coords[i][j]);
-			if (j != 2)
-				str += ", ";
-		}
-		str += ")";
-		if (i != 7)
-			str += "; ";
-	}
+		str += "(" + std::to_string(vertices[i].x) + ", " + std::to_string(vertices[i].y) + ", " + std::to_string(vertices[i].z) + ")\n";
 	return str;
 }
 
