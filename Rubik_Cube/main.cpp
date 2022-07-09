8ecf84b2357f97eaf6a1afdea01d86bd1b9e95b9
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-07-08 19:50:35 -0500

    feat: Camera rotations a translations

--- Rubik_Cube/main.cpp
+++ Rubik_Cube/main.cpp
@@ -8,51 +8,29 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
+#include <string>
+
+#include "Rubik.h"
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 float getDeltaTime();
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-// camera system
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+const unsigned int SCR_WIDTH = 625;
+const unsigned int SCR_HEIGHT = 625;
 
 // timing
 //float deltaTime = 0.0f;	// time between current frame and last frame
 //float lastFrame = 0.0f;
 
-float xRot = 0.0f ;
-float yRot = 0.0f ;
-float zRot = 0.0f ;
+//float xRot = 0.0f ;
+//float yRot = 0.0f ;
+//float zRot = 0.0f ;
 
-unsigned int VBO_array[12];
-unsigned int VAO_array[12];
 
-float vertices_[12][9] = {
-    // bottom
-    {-0.5f, -0.5f, 0.5f,      0.5f, -0.5f, 0.5f,    0.5f, -0.5f, -0.5f},
-    {0.5f, -0.5f, -0.5f,    -0.5f, -0.5f, -0.5f,    -0.5f, -0.5f, 0.5f},
-    // top
-    {-0.5f, 0.5f, 0.5f,      0.5f, 0.5f, 0.5f,    0.5f, 0.5f, -0.5f},
-    {0.5f, 0.5f, -0.5f,      -0.5f, 0.5f, -0.5f,    -0.5f, 0.5f, 0.5f},
-    // left
-    {-0.5f, -0.5f, -0.5f,      -0.5f, 0.5f, -0.5f,    -0.5f, 0.5f, 0.5f},
-    {-0.5f, 0.5f, 0.5f,      -0.5f, -0.5f, 0.5f,    -0.5f, -0.5f, -0.5f},
-    // right
-    {0.5f, -0.5f, -0.5f,      0.5f, 0.5f, -0.5f,    0.5f, -0.5f, 0.5f},
-    {0.5f, 0.5f, 0.5f,      0.5f, -0.5f, 0.5f,    0.5f, 0.5f, -0.5f},
-    // back
-    {-0.5f, -0.5f, -0.5f,      0.5f, -0.5f, -0.5f,    -0.5f, 0.5f, -0.5f},
-    {0.5f, -0.5f, -0.5f,      0.5f, 0.5f, -0.5f,    -0.5f, 0.5f, -0.5f},
-    // front
-    {-0.5f, -0.5f, 0.5f,      0.5f, -0.5f, 0.5f,    0.5f, 0.5f, 0.5f},
-    {-0.5f, -0.5f, 0.5f,      0.5f, 0.5f, 0.5f,    -0.5f, 0.5f, 0.5f}
-};
+Rubik rubik;
 
 
 int main()
@@ -84,47 +62,13 @@ int main()
 
     Shader shader{ "shader" };
 
-    // unsigned int VBO, VAO, EBO;
-    for (int i = 0; i < 12; i++)
-    {
-        glGenVertexArrays(1, &VAO_array[i]);
-        glGenBuffers(1, &VBO_array[i]);
-        // glGenBuffers(1, &EBO);
-        glBindVertexArray(VAO_array[i]);
+    rubik.prepare_VB0_VAO();
+  
 
-        glBindBuffer(GL_ARRAY_BUFFER, VBO_array[i]);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_[i]), vertices_[i], GL_STATIC_DRAW);
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-        // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-
-        //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-        //glEnableVertexAttribArray(0);
-
-        //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-        //glEnableVertexAttribArray(1);
-
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
-    }
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    glm::mat4 proj = glm::perspective(glm::radians(60.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
     shader.setMat4("projection", proj);
-
-
-    glm::vec3 colors[] =
-    {
-        glm::vec3(1.0f, 1.0f, 0.0f), // YELLOW
-        glm::vec3(1.0f, 1.0f, 1.0f), // WHITE
-        glm::vec3(0.0f, 1.0f, 0.0f), // GREEN
-        glm::vec3(0.0f, 0.0f, 1.0f), // BLUE
-        glm::vec3(1.0f, 0.5f, 0.0f), // ORANGE
-        glm::vec3(1.0f, 0.0f, 0.0f)  // RED
-    };
 
     glEnable(GL_DEPTH_TEST);
     //glEnable(GL_CULL_FACE);
@@ -138,46 +82,12 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        
-        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, xRot, glm::vec3(1.0f, 0.0f, 0.0f));
-        model = glm::rotate(model, yRot, glm::vec3(0.0f, 1.0f, 0.0f));
-        model = glm::rotate(model, zRot, glm::vec3(0.0f, 0.0f, 1.0f));
-        glm::mat4 MVP = proj * view * model;
-        
-        shader.use();
-        shader.setMat4("view", view);
-        shader.setMat4("u_MVP", MVP);
-
-        for (int i = 0; i < 12; i += 2)
-        {
-            shader.setVec3("u_faceColor", colors[i / 2]);
-            glBindVertexArray(VAO_array[i]);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
-            shader.setVec3("u_faceColor", colors[i / 2]);
-            glBindVertexArray(VAO_array[i+1]);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
-        }
-        //for (int i = 0; i < 6; i++)
-        //{
-         //   shader.setVec3("u_faceColor", colors[i]);
-            //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(i * 6 * sizeof(GLuint)));
-        //}
-
-
+        rubik.draw(shader, proj);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    for (int i = 0; i < 12; i++)
-    {
-        glDeleteVertexArrays(1, &VAO_array[i]);
-        glDeleteBuffers(1, &VBO_array[i]);
-        // glDeleteBuffers(1, &EBO);
-    }
     glfwTerminate();
     return 0;
 }
@@ -188,41 +98,91 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
     
-    // Cube Rotations
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        xRot -= deltaTime;
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        xRot += deltaTime;
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        yRot -= deltaTime;
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        yRot += deltaTime;
+    // Cube movements 
     if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-        zRot += deltaTime;
+        rubik.rotate(rotation_type::LEFT, false);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        rubik.rotate(rotation_type::LEFT, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        rubik.rotate(rotation_type::CENTER_X, false);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        rubik.rotate(rotation_type::CENTER_X, true);
+
     if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
-        zRot -= deltaTime;
+        rubik.rotate(rotation_type::RIGHT, false);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        rubik.rotate(rotation_type::RIGHT, true);
+
+
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
+        rubik.rotate(rotation_type::TOP, false);
+    if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
+        rubik.rotate(rotation_type::TOP, true);
+
+    if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS)
+        rubik.rotate(rotation_type::CENTER_Y, false);
+    if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS)
+        rubik.rotate(rotation_type::CENTER_Y, true);
+
+    if (glfwGetKey(window, GLFW_KEY_Y) == GLFW_PRESS)
+        rubik.rotate(rotation_type::BOTTOM, false);
+    if (glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS)
+        rubik.rotate(rotation_type::BOTTOM, true);
+
+
+    if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
+        rubik.rotate(rotation_type::FRONT, false);
+    if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
+        rubik.rotate(rotation_type::FRONT, true);
+
+    if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
+        rubik.rotate(rotation_type::CENTER_Z, false);
+    if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
+        rubik.rotate(rotation_type::CENTER_Z, true);
+
+    if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS)
+        rubik.rotate(rotation_type::BACK, false);
+    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
+        rubik.rotate(rotation_type::BACK, true);
+
+
 
     // Camera movements
-    const float cameraSpeed = 0.001f; // adjust accordingly
+    const float cameraSpeed = 0.01f; // adjust accordingly
 
     // Camera front movement
-    if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_KP_6) == GLFW_PRESS)
         cameraPos += cameraSpeed * cameraFront;
     // Camera back movement
-    if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_KP_4) == GLFW_PRESS)
         cameraPos -= cameraSpeed * cameraFront;
     // Camera left movement
-    if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_KP_1) == GLFW_PRESS)
         cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
     // Camera right movement
-    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_KP_3) == GLFW_PRESS)
         cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
     // Camera up movement
-    if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
-        cameraPos += cameraSpeed * cameraUp;
-    // Camera down movement
-    if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_KP_5) == GLFW_PRESS)
         cameraPos -= cameraSpeed * cameraUp;
+    // Camera down movement
+    if (glfwGetKey(window, GLFW_KEY_KP_2) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraUp;
+
+    // Camera rotations
+    if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
+        xRot -= deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
+        xRot += deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS)
+        yRot -= deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_V) == GLFW_PRESS)
+        yRot += deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS)
+        zRot += deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS)
+        zRot -= deltaTime;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
